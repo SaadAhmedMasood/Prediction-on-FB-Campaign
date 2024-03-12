93d49a70427bdeaf0d8e846ce5fa811fa15ad47c
Saad Ahmed Masood <145034934+SaadAhmedMasood@users.noreply.github.com>
2024-03-12 20:03:47 +0500

    Update fbstream.py
    Without .pkl file

--- fbstream.py
+++ fbstream.py
@@ -1,9 +1,41 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+#import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
 import streamlit as st
 import pandas as pd
-import joblib
+#import joblib
 import random
 
-pipeline = joblib.load('trained_pipeline_DT.pkl')
+df = pd.read_excel('Social_FB.xlsx')
+
+X = df.drop(columns=['approved_conversion'])
+y = df['approved_conversion']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+categorical_features = ['gender', 'ad_region']
+categorical_features = [feature for feature in categorical_features if feature in X.columns]
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('cat', OneHotEncoder(), categorical_features)
+    ])
+
+pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                           ('classifier', DecisionTreeClassifier(random_state=42))])
+
+pipeline.fit(X_train, y_train)
+y_pred = pipeline.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)*100
+#print("Accuracy:", accuracy)
+
+#joblib.dump(pipeline, 'trained_pipeline_DT.pkl')
+
+#pipeline = joblib.load('trained_pipeline_DT.pkl')
 
 # Function to preprocess user input
 def preprocess_input(user_input):
